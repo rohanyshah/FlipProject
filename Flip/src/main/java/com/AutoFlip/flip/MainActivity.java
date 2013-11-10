8150fc8c5e2d99cee0cf899ad42aeb4db6b1ed42
Steve Vitali <14stevevitali@gmail.com>
2013-11-09 19:25:34 -0500

    Add file import methods

--- Flip/src/main/java/com/AutoFlip/flip/MainActivity.java
+++ Flip/src/main/java/com/AutoFlip/flip/MainActivity.java
@@ -1,7 +1,10 @@
 package com.AutoFlip.flip;
 
+import android.os.Environment;
+
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,13 +17,22 @@ import java.util.Map;
 import java.text.SimpleDateFormat;
 
 import org.apache.pdfbox.util.PDFTextStripper;
-import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 
 import java.io.File;
 
 import android.os.Build;
 import java.io.IOException;
 import java.io.InputStream;
+
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.util.Log;
+import java.io.FilenameFilter;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.os.Environment;
 
 public class MainActivity extends Activity {
 
@@ -73,10 +85,64 @@ public class MainActivity extends Activity {
         }
     }
 
+    //In an Activity
+    private String[] mFileList;
+
+    private File mPath = new File(Environment.getExternalStorageDirectory() + "//yourdir//");
+    private String mChosenFile;
+    private static final String FTYPE = ".txt";
+    private static final int DIALOG_LOAD_FILE = 1000;
+
     public void importCards(View view)
     {
-        File pdf = new File();
+
     }
+
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new Builder(this);
+
+        switch(id) {
+            case DIALOG_LOAD_FILE:
+                builder.setTitle("Choose your file");
+                if(mFileList == null) {
+                    Log.e("", "Showing file picker before loading the file list");
+                    dialog = builder.create();
+                    return dialog;
+                }
+                builder.setItems(mFileList, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mChosenFile = mFileList[which];
+                        //you can do stuff with the file here too
+                    }
+                });
+                break;
+        }
+        dialog = builder.show();
+        return dialog;
+    }
+
+    private void loadFileList() {
+        try {
+            mPath.mkdirs();
+        }
+        catch(SecurityException e) {
+            Log.e("", "unable to write on the sd card " + e.toString());
+        }
+        if(mPath.exists()) {
+            FilenameFilter filter = new FilenameFilter() {
+                public boolean accept(File dir, String filename) {
+                    File sel = new File(dir, filename);
+                    return filename.contains(FTYPE) || sel.isDirectory();
+                }
+            };
+            mFileList = mPath.list(filter);
+        }
+        else {
+            mFileList= new String[0];
+        }
+    }
+
 
     private String processPDF(File file, String outputDateMask, Map<String, String> metadata) throws Throwable {
         PDFTextStripper pdfS = new PDFTextStripper();
